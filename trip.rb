2508dd840226599a72f615f1bc2da39e5eaccaed
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-20 19:00:25 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -5,9 +5,9 @@ puts "What sites would you like to vist?"
 sites= gets.chomp
 sites.capitalize!
 puts "What type of food do you want to eat?"
-food= gets.chomp
-food.capitalize!
+eat= gets.chomp
+eat.capitalize!
 puts "How long do you want to stay?"
 stay=gets.chomp
 stay.capitalize!
-puts "Greetings! Thanks for booking with Gibson Travel, you've been booked for a trip to #{city} for #{stay} nights. You're all set to visit the #{sites}, and we have all the #{food} your tummy can bear! "
+puts "Greetings! Thanks for booking with Gibson Travel, you've been booked for a trip to #{city} for #{stay} nights. You're all set to visit the #{sites}, and we have all the #{eat} your tummy can bear! "
