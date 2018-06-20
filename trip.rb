3eaedce71aefd5f45f799605b3750326e38e242d
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-20 18:55:56 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,2 +1,13 @@
 puts "Lets plan a Trip! Where would you like to stay?"
 city=gets.chomp
+city.capitalize!
+puts "What sites would you like to vist?"
+sites= gets.chomp
+sites.capitalize!
+puts "What type of food do you want to eat?"
+food= gets.chomp
+food.capitalize!
+puts "How long do you want to stay?"
+stay=gets.chomp
+stay.capitalize!
+puts "Greetings! Thanks for booking with Gibson Travel, you've been booked "
