71387768fca5afe76618f469a918e0334df6e7f5
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-20 18:58:25 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -10,4 +10,4 @@ food.capitalize!
 puts "How long do you want to stay?"
 stay=gets.chomp
 stay.capitalize!
-puts "Greetings! Thanks for booking with Gibson Travel, you've been booked "
+puts "Greetings! Thanks for booking with Gibson Travel, you've been booked for a trip to #{city} for #{stay} nights. You're all set to visit the #{sites}, and we have all the #{food} your tummy can bear! "
